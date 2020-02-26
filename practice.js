67e71a3c39ff9b785e1dea2727a9b165d5622ac1
Trevor Ramey <treramey@gmail.com>
2020-02-26 15:44:58 -0600

    Completed section

--- practice.js
+++ practice.js
@@ -19,6 +19,9 @@ var arr = [10,20,30];
 
 //Code Here
 
+function first(arr) {
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -34,6 +37,10 @@ var arr = [40,50,60];
 
 //Code Here
 
+function last(arr) {
+  return arr[arr.length - 1];
+}
+
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +55,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family) {
+  for (let index = 0; index < family.length; index++) {
+    alert(family[index]);
+  }
+};
 
 
 ////////// PROBLEM 4 //////////
@@ -63,6 +74,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters) {
+  for (let index = letters.length-1; index >= 0; index--) {
+    alert(letters[index]);
+  }
+}
 
 
 
@@ -79,7 +95,20 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
+function evenFinder(nums) {
+  let even = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0){
+      even.push(nums[i]);
+    } 
+  
+  }
+  return even
 
+}
+
+var test = evenFinder(nums);
+console.log(test);
 
 
 
@@ -107,7 +136,21 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
-
+function divider(numbersArray) {
+  even = [];
+  odd = [];
+  comb = [even,odd]
+  for (let i = 0; i < numbersArray.length; i++) {
+    if(numbersArray[i] % 2 === 0){
+      even.push(numbersArray[i]);
+    } else {
+      odd.push(numbersArray[i]);
+    }
+  }
+  return comb ;
+}
+ var div = divider(numbersArray)
+ console.log(div)
 
 
 ////////// PROBLEM 7 //////////
@@ -129,7 +172,10 @@ var getRandomArbitrary = function() {
 */
 
 //Code Here
-
+function finder(array) {
+  let randomNumber  = getRandomArbitrary();
+  return array.includes(randomNumber)
+  }
 
 
 ////////// PROBLEM 8 //////////
@@ -158,7 +204,6 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 */
 
 //Code Here
-
 
 
 ////////// PROBLEM 9 //////////
